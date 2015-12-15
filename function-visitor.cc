1a41c9a2c98eb1cdd7a8234e5967bd3e3fa061d5
Bastian Rieck <bastian@rieck.ru>
2015-12-15 21:17:40 +0100

    Added support for compilation databases
    So far, this is getting me nowhere but it appears as though the
    database is at least parsed correctly right now.
    
    This feels very clunky but of course it makes sense---given a
    complicated language such as C++, how should a simple program
    know what AST to build without compilation commands?

--- function-visitor.cc
+++ function-visitor.cc
@@ -1,7 +1,25 @@
+#include <clang-c/CXCompilationDatabase.h>
 #include <clang-c/Index.h>
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+
+#include <limits.h>
+#include <stdlib.h>
+
+std::string resolvePath( const char* path )
+{
+  std::string result;
+
+  char* resolvedPath = new char[PATH_MAX];
+  auto resolved      = realpath( path, resolvedPath );
+  if( resolved )
+    result = resolvedPath;
+
+  delete[] resolvedPath;
+  return result;
+}
 
 CXChildVisitResult functionVisitor( CXCursor cursor, CXCursor parent, CXClientData clientData )
 {
@@ -32,14 +50,61 @@ CXChildVisitResult functionVisitor( CXCursor cursor, CXCursor parent, CXClientDa
 
 int main( int argc, char** argv )
 {
+  if( argc < 2 )
+    return -1;
+
+  auto resolvedPath = resolvePath( argv[1] );
+  std::cout << "Parsing " << resolvedPath << "...\n";
+
+  CXCompilationDatabase_Error compilationDatabaseError;
+  CXCompilationDatabase compilationDatabase = clang_CompilationDatabase_fromDirectory( ".", &compilationDatabaseError );
+
+  CXCompileCommands compileCommands         = clang_CompilationDatabase_getCompileCommands( compilationDatabase, resolvedPath.c_str() );
+
+  unsigned int numCompileCommands = clang_CompileCommands_getSize( compileCommands );
+  std::cout << "I have obtained " << numCompileCommands << " compile commands\n"; 
+
+  if( numCompileCommands == 0 )
+    return -1;
+
+  // FIXME: Get the first compile command. I can always extend this later...
+  CXCompileCommand compileCommand   = clang_CompileCommands_getCommand( compileCommands, 0 );
+
+  unsigned int numArguments = clang_CompileCommand_getNumArgs( compileCommand );
+  std::cout << "Selected compile command has " << numArguments << " arguments\n";
+
+  // FIXME: This is as leaky as the roof in the shed at my parents where I used
+  // to live
+  char** arguments = new char*[ numArguments ];
+
+  for( unsigned int i = 0; i < numArguments; i++ )
+  {
+    CXString argument = clang_CompileCommand_getArg( compileCommand, i );
+
+    std::string strArgument = clang_getCString( argument ); 
+    arguments[i]            = new char[ strArgument.size() + 1 ];
+    
+    std::fill( arguments[i], arguments[i] + strArgument.size() + 1, 0 );
+
+    std::copy( strArgument.begin(), strArgument.end(),
+               arguments[i] );
+
+    std::cout << "  " << strArgument << "\n";
+
+    clang_disposeString( argument ); 
+  }
+
   CXIndex index                     = clang_createIndex( false, true );
-  CXTranslationUnit translationUnit = clang_parseTranslationUnit( index, 0, argv, argc, 0, 0, CXTranslationUnit_None );
+  CXTranslationUnit translationUnit = clang_parseTranslationUnit( index, 0, arguments, numArguments, 0, 0, CXTranslationUnit_None );
 
   CXCursor rootCursor = clang_getTranslationUnitCursor( translationUnit );
   clang_visitChildren( rootCursor, functionVisitor, nullptr );
 
+  clang_CompileCommands_dispose( compileCommands );
+
   clang_disposeTranslationUnit( translationUnit );
   clang_disposeIndex( index );
 
+  clang_CompilationDatabase_dispose( compilationDatabase );
   return 0;
 }
