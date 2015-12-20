3a5926b62344e732b74a9bba9d5d99a107f957f1
Bastian Rieck <bastian@rieck.ru>
2015-12-20 18:55:53 +0100

    Added test programs for "99 bottles of beer"

---
+++ examples/99_recursive.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void rec(int bottles)
+{
+  if ( bottles!=0)    
+  {    
+    cout << bottles << " bottles of beer on the wall" << endl; 
+    cout << bottles << " bottles of beer" << endl;
+    cout << "Take one down, pass it around" << endl; 
+    cout << --bottles << " bottles of beer on the wall\n" << endl;    
+    rec(bottles);
+  }  
+}
+
+int main() 
+{   
+  rec(99);
+  return 0;
+}
